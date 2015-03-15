33bdd98243fa6649f51d0cb41e4cf63c0fec9e28
MadisonRong <rongweixin041@gmail.com>
2015-03-15 14:22:29 +0800

    devise business

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :businesses
   devise_for :admins
   resources :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315061929) do
+ActiveRecord::Schema.define(version: 20150315062141) do
 
   create_table "admins", force: :cascade do |t|
     t.datetime "created_at",                                      null: false
@@ -39,15 +39,28 @@ ActiveRecord::Schema.define(version: 20150315061929) do
   add_index "admins_roles", ["admin_id", "role_id"], name: "index_admins_roles_on_admin_id_and_role_id", using: :btree
 
   create_table "businesses", force: :cascade do |t|
-    t.string   "name",               limit: 255,             null: false
-    t.string   "operating_license",  limit: 255,             null: false
-    t.string   "legal_person_name",  limit: 255,             null: false
-    t.string   "legal_person_photo", limit: 255,             null: false
-    t.integer  "business_status",    limit: 4,   default: 0
-    t.integer  "platform_admin_id",  limit: 4
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.string   "name",                   limit: 255,              null: false
+    t.string   "operating_license",      limit: 255,              null: false
+    t.string   "legal_person_name",      limit: 255,              null: false
+    t.string   "legal_person_photo",     limit: 255,              null: false
+    t.integer  "business_status",        limit: 4,   default: 0
+    t.integer  "platform_admin_id",      limit: 4
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
+
+  add_index "businesses", ["email"], name: "index_businesses_on_email", unique: true, using: :btree
+  add_index "businesses", ["reset_password_token"], name: "index_businesses_on_reset_password_token", unique: true, using: :btree
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id",     limit: 4,               null: false
